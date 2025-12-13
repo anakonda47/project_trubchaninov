618c3702be4073e08b574df856fce5284b3f5c31
anakonda47 <zanakonda47@gmail.com>
2025-12-13 15:23:43 +0300

    Create PZ_7.2.py

---
+++ PZ_7/PZ_7.2.py
@@ -0,0 +1,10 @@
+#Даны строки S, S1 и S2. Заменить в строке S все вхождения строки Si на строку S2.
+try:
+    S = input()
+    S1 = input()
+    S2 = input()
+
+    result = S.replace(S1, S2)
+    print(result)
+except ValueError as e:
+    print(e)

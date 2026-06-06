0e11ca57e663abf80c5c6fabbbd3b15881eb5509
anakonda47 <zanakonda47@gmail.com>
2026-06-06 17:05:07 +0300

    Create pz_11.2.py

---
+++ PZ_11/pz_11.2.py
@@ -0,0 +1,18 @@
+2. Из списка: ['Валентин', 'Петр', 'Анна', 'Евгений', 'Константин', 'Валерия', 'Юлия']
+получить новый список, в котором длина слов не превышает 5 символов."""
+
+try:
+    # функция-генератор
+    def filter_names(names_list):
+        for name in names_list:
+            if len(name) <= 5:
+                yield name
+
+    input_list = ['Валентин', 'Петр', 'Анна', 'Евгений', 'Константин', 'Валерия', 'Юлия']
+    print("Исходный список:", input_list)
+
+    result_list = list(filter_names(input_list))
+    print("Новый список (длина слов не превышает 5 символов):", result_list)
+
+except ValueError as e:
+    print(e)

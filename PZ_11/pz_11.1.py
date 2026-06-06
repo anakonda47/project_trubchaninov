d6e77531b7df4ff7394116e75f36f4a8e99920cf
anakonda47 <zanakonda47@gmail.com>
2026-06-06 17:04:11 +0300

    Create pz_11.1.py

---
+++ PZ_11/pz_11.1.py
@@ -0,0 +1,25 @@
+1. В последовательности на n целых элементов в последней ее половине найти
+сумму элементов."""
+
+import random
+
+def main():
+    try:
+        a = int(input("Напишите кол-во чисел\n"))
+        
+        # генерация списка
+        b = [random.randint(-10, 10) for _ in range(a)]
+        print("Сам список:", b)
+        
+        # срез второй половины и сумма
+        half_index = len(b) // 2
+        second_half = b[half_index:]
+        print("Последняя половина элементов:", second_half)
+        
+        total_sum = sum(second_half)
+        print(f"Сумма элементов в последней половине: {total_sum}")
+
+    except ValueError as e:
+        print(f"Произошла ошибка: {e}")
+
+main()

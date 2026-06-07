b89492b643a7b45f122c74c42c2ecffcc7902093
anakonda47 <zanakonda47@gmail.com>
2026-06-07 13:40:58 +0300

    Update pz_10.1.py

--- PZ_10/pz_10.1.py
+++ PZ_10/pz_10.1.py
@@ -1,7 +1,7 @@
-1. Средствами языка Python сформировать два текстовых файла (.txt), содержащих по одной
-последовательности из целых положительных и отрицательных чисел. Сформировать
-новый текстовый файл (.txt) следующего вида, предварительно выполнив требуемую
-обработку элементов:
+#1. Средствами языка Python сформировать два текстовых файла (.txt), содержащих по одной
+#последовательности из целых положительных и отрицательных чисел. Сформировать
+#новый текстовый файл (.txt) следующего вида, предварительно выполнив требуемую
+#обработку элементов:
 
 Содержимое первого файла:
 Четные элементы:
@@ -20,20 +20,20 @@ try:
         for i in f.read().split(","):
             list.append(int(i))
             
-    # четные элементы
+   
     even_elements = [i for i in list if i % 2 == 0]
     
-    # произведение четных
+
     mult_even = 1
     for i in even_elements:
         mult_even *= i
     if not even_elements:  # на случай если четных нет
         mult_even = 0
         
-    # минимальный элемент
+  
     min_element = min(list)
 
-    # сохраняем первую часть в result.txt
+   
     with open("result.txt", "w", encoding="utf-8") as f:
         f.write(f"Содержимое первого файла: {list}\n\n"
                 f"Четные элементы: {even_elements}\n\n"
@@ -42,19 +42,19 @@ try:
 
 
     # --- ОБРАБОТКА ВТОРОГО ФАЙЛА ---
-    list = []  # очищаем список под второй файл
+    list = []  
     with open("text2.txt", "r", encoding="utf-8") as f:
         for i in f.read().split(","):
             list.append(int(i))
             
-    # нечетные элементы
+
     odd_elements = [i for i in list if i % 2 != 0]
     
-    # кол-во и сумма нечетных
+ 
     count_odd = len(odd_elements)
     sum_odd = sum(odd_elements)
 
-    # дописываем вторую часть в тот же файл result.txt (режим "a" - append)
+    
     with open("result.txt", "a", encoding="utf-8") as f:
         f.write(f"Содержимое второго файла: {list}\n\n"
                 f"Нечетные элементы: {odd_elements}\n\n"
